9178598414ffea95fe214095ad7ca3fefc9d46f2
Revant Gupta <revant@DESKTOP-I8VFGQF>
2023-04-16 05:18:14 +0200

    Placed common methods in separate files.

---
+++ switchstate/models/trainer.py
@@ -0,0 +1,145 @@
+import time
+import torch
+from tqdm.auto import tqdm
+
+from ..utils import log_domain_mean   
+from .methods import compute_log_likelihood   
+
+def train(self, D, TPM=None, num_epochs=300, sparsity_weight=1.0,
+            optimizer=None, criterion=None, swa_scheduler=None, swa_start=200, 
+            verbose=False):
+    
+    '''
+    Train the model.
+    
+    Parameters
+    ----------
+    D : FloatTensor of shape (MSM_nodes, T_max)
+        MSM simulation data.
+    TPM : Transition probability matrix
+        Used to regularise emission probabilities.
+    num_epochs : int (default: 300)
+        Number of training epochs
+    sparsity_weight : float (default: 1.0)
+        Regularisation weight for sparse latent TPM.
+    optimizer : (default: RMSProp(lr=0.2))
+        Optimizer algorithm.
+    criterion : (default: KLDivLoss())
+        Loss function. (Default preferred)
+    swa_scheduler : (default: None)
+        SWA scheduler.
+    swa_start : int (default: 200)
+        Training epoch to start SWA.
+    verbose : bool (default: False)
+        Toggle printing of training history
+    
+    '''
+
+    if self.is_cuda:
+        D = D.cuda()
+    
+    self.sparsity_weight = sparsity_weight
+        
+    try: assert self.elapsed_epochs
+    except: self.elapsed_epochs = 0
+    
+    try: assert self.loss_values
+    except: self.loss_values = []
+
+    try: assert self.divergence_values
+    except: self.divergence_values = []
+        
+    try: assert self.likelihood_values
+    except: self.likelihood_values = []
+
+    try: assert self.sparsity_values
+    except: self.sparsity_values = []
+
+    try: assert self.regularisation_values
+    except: self.regularisation_values = []
+
+    try: assert self.independence_values
+    except: self.independence_values = []
+    
+    self.optimizer = optimizer
+    if self.optimizer is None:
+        self.optimizer = torch.optim.RMSprop(self.parameters(), lr=0.2)
+    self.swa_scheduler = swa_scheduler
+    self.criterion = criterion
+    if self.criterion is None:
+        self.criterion = torch.nn.KLDivLoss(reduction='batchmean', log_target=False)
+            
+    start_time = time.time()           
+    for epoch in tqdm(range(num_epochs), desc='Training dynamic model'):
+
+        self.optimizer.zero_grad()
+        prediction, log_observed_state_probs_, log_hidden_state_probs = self.forward_model()
+        log_alpha, log_probs = self.filtering(D)
+        
+        divergence = self.criterion(prediction, D)
+        loss = divergence
+        self.divergence_values.append(divergence.item())
+
+        compute_log_likelihood(self)
+        self.likelihood_values.append(self.log_likelihood.item())
+
+        sparsity = self.criterion(self.log_transition_matrix, torch.eye(self.num_states))
+        loss += self.sparsity_weight*sparsity
+        self.sparsity_values.append(sparsity.item())
+                        
+        # independence = (log_domain_mean(log_observed_state_probs_, 0).logsumexp(1) - \
+        #                 log_domain_mean(log_observed_state_probs_, 0).logsumexp(1).logsumexp(0)).sum(0).logsumexp(0)
+        
+        # independence = (log_domain_mean(log_observed_state_probs_, 0).logsumexp(0) - \
+        #                 log_domain_mean(log_observed_state_probs_, 0).logsumexp(0).logsumexp(0)).sum(0).logsumexp(0)
+
+        independence = log_domain_mean(log_observed_state_probs_, 0).logsumexp(1).sum(-1).logsumexp(0)
+        #independence = self.log_emission_matrix.logsumexp(1).sum(0).logsumexp(-1)
+        
+        loss -= 1/independence
+        self.independence_values.append(independence.item())
+
+        if TPM is not None:
+            regularisation = self.criterion(self.log_emission_matrix, 
+                                            torch.matmul(torch.exp(self.log_emission_matrix.detach()), TPM))
+            #loss += regularisation
+            self.regularisation_values.append(regularisation.item())
+
+        loss.backward()
+        self.optimizer.step()
+        if self.elapsed_epochs > swa_start and self.swa_scheduler is not None:
+            swa_scheduler.step()
+        
+        self.loss_values.append(loss.item())
+
+        self.elapsed_epochs += 1
+        if epoch % 10 == 0 or epoch <=10:
+            corrcoeffs = []
+            outputs = torch.exp(prediction)
+            for t in range(self.num_iters):
+                corrcoeffs.append(torch.corrcoef(torch.stack([outputs[t], D[t]]))[1,0])
+            avg_corrcoeff = torch.mean(torch.tensor(corrcoeffs))
+
+            # Print Loss
+            if verbose:
+                if TPM is not None:
+                    print('{:.2f}s. It {} Loss {:.2E} KL {:.2E} Likl {:.2E} Sparse {:.2E} Reg {:.2E} Exl {:.2E} Corcoef {:.2f}'.format(time.time() - start_time,
+                                                                                self.elapsed_epochs, 
+                                                                                self.loss_values[-1],
+                                                                                self.divergence_values[-1],
+                                                                                self.likelihood_values[-1],
+                                                                                self.sparsity_values[-1],
+                                                                                self.regularisation_values[-1],
+                                                                                self.independence_values[-1],
+                                                                                avg_corrcoeff))
+                else:
+                    print('{:.2f}s. It {} Loss {:.2E} KL {:.2E} Likl {:.2E} Sparse {:.2E} Exl {:.2E} Corcoef {:.2f}'.format(time.time() - start_time,
+                                                                                self.elapsed_epochs, 
+                                                                                self.loss_values[-1],
+                                                                                self.divergence_values[-1],
+                                                                                self.likelihood_values[-1],                                                    
+                                                                                self.sparsity_values[-1],
+                                                                                self.independence_values[-1],
+                                                                                avg_corrcoeff))
+            
+        
