e83a623bf7d449c16e8475abc0131c179f92c7a2
Revant Gupta <revant@DESKTOP-I8VFGQF>
2023-04-17 16:12:11 +0200

    Implemented function to summarise lineage probabilities.

--- switchstate/plotting/plot_dynamics.py
+++ switchstate/plotting/plot_dynamics.py
@@ -7,6 +7,7 @@ from matplotlib import pyplot as plt
 from matplotlib.colors import PowerNorm
 
 from ..pseudotime import infer_pseudotime
+from .plot_cluster_probabilities import plot_probability_by_clusters
 
 # Plot model loss
 def plot_loss(model, figsize=(15,7)):
@@ -157,7 +158,8 @@ def plot_static_kinetic_overlap(adata, cluster_key: str, states=None, ax=None):
     probs[cluster_key] = adata.obs[cluster_key]
     probs = probs.groupby(cluster_key).mean()
 
-    sns.heatmap(probs.T.astype(float), annot=True, cmap='YlGnBu', ax=ax)
+    sns.heatmap(probs.T.astype(float), annot=True, cmap='YlGnBu', 
+                vmax=1, vmin=0, ax=ax)
     ax.set_yticklabels(states)
     ax.set_xlabel('')
     ax.set_title('Annotation - States overlap')
@@ -229,19 +231,20 @@ def plot_dynamics_summary(adata, cluster_key: str, use_selected=True,
     axs.flat[10].set_xticklabels(axs.flat[10].get_xticklabels(), rotation=rotation)
 
     # Annotation, kinetic entropy overlap
-    entropy_df = adata.obs.groupby(['kinetic_states', cluster_key]).mean().reset_index()
+    entropy_df = adata.obs.groupby(['kinetic_states', cluster_key]).sum().reset_index()
     entropy_df = entropy_df.pivot(index='kinetic_states', columns=cluster_key, 
                                   values='cellular_entropy').astype(float).round(2)
-    #entropy_df = entropy_df.divide(entropy_df.sum(1), axis=0)
+    entropy_df = entropy_df.divide(adata.obs.groupby(cluster_key).count().cellular_entropy, axis=1)
 
     sns.heatmap(entropy_df, 
                 cmap='YlGnBu', annot=True, 
                 ax=axs.flat[11])
-    axs.flat[11].set_title('Mean Entropy')
+    axs.flat[11].set_title('Entropy of annotation per states')
 
     return fig, axs 
 
 # Dynamics report - emissions
+
 # Plot argmax latent states, memberships and probability
 def plot_latent_summary(adata, use_selected=True, ncols=4, basis='umap', figsize=(5, 3.5)):
 
@@ -275,24 +278,93 @@ def plot_latent_summary(adata, use_selected=True, ncols=4, basis='umap', figsize
             scatter(adata, color='silver', basis=basis, ax=axs.flat[i], show=False)
             scatter(adata, color=probs[int(i/2)], color_map='YlGnBu', 
                     title='Hidden state prob. {}'.format(selected_states[int(i/2)]), 
-                    basis=basis, ax=axs.flat[i], show=False)
+                    basis=basis, vmin=0, vmax=1, ax=axs.flat[i], show=False)
             
             scatter(adata, color='silver', basis=basis, ax=axs.flat[i+1], show=False)
-            scatter(adata, color=pd.get_dummies(probs.argmax(0))[int(i/2)], color_map='YlGnBu', 
+            scatter(adata, color=pd.get_dummies(probs.argmax(0))[int(i/2)], color_map='binary', 
                     title='Hidden state memb. {}'.format(selected_states[int(i/2)]), 
                     basis=basis, ax=axs.flat[i+1], show=False)
 
     return fig, axs
 
 # Dynamics report - lineages
+
 # Heatmap with latent state history
-def plot_latent_state_history():
-    raise NotImplementedError()
+def plot_latent_state_history(adata, states=None, chain=None, ax=None):
+    
+    # Check if probs exist
+    try: assert 'joint_probabilities' in adata.uns['latent_dynamics']['model_outputs'].keys()
+    except: raise ValueError('Probabilities not found. Run infer_latent_dynamics() first')
+
+    probs = adata.uns['latent_dynamics']['model_outputs']['joint_probabilities']
+    if states is None:
+        states = np.arange(probs.shape[1])
+    probs = probs[:, states]
+
+    # Hidden State probabilities chain
+    if chain is None:
+        probs = probs.sum(-1).sum(-1).T
+    else:
+        probs = probs.sum(-1)[:,:,chain].T
+
+    sns.heatmap(probs, cmap='Blues', ax=ax)
+
+    return ax
 
 # Plot lineages panel
-def plot_lineages_summary():
-    # Cell fate - annot overlap (?)
-    # Hidden history
-    # Annotation -> observed history
-    raise NotImplementedError()  
+def plot_lineages_summary(adata, cluster_key: str, use_selected=True, 
+                          basis='umap', rotation=90, figsize=(25, 5)):
+    
+    # Check if cluster key exists
+    if cluster_key not in adata.obs.columns:
+        raise KeyError('Key {} for cluster annotations not found in adata.obs.'.format(cluster_key))
+
+    if use_selected:
+        try: assert 'selected_states' in adata.uns['latent_dynamics']['posthoc_computations'].keys()
+        except: raise ValueError('No selected states. Run latent_state_selection() first')
+        selected_states = adata.uns['latent_dynamics']['posthoc_computations']['selected_states']
+    else:
+        selected_states = None
+
+    num_chains = adata.uns['latent_dynamics']['latent_dynamics_params']['num_chains']
+
+    figsize = (figsize[0], figsize[1]*num_chains)
+    fig, axs = plt.subplots(ncols=4, nrows=num_chains, figsize=figsize)
+
+    for i in range(num_chains):
+    
+        # Plot lineage membership
+        scatter(adata, color='silver', basis=basis, ax=axs.flat[i*4], show=False)
+        scatter(adata, basis=basis, 
+                       color=adata.obs.kinetic_states.isin(np.array(adata.uns['latent_dynamics']['posthoc_computations']['condensed_latent_paths'][i]).astype(str)),
+                       title='Chain {} cell membership'.format(i), 
+                       color_map='binary', show=False, ax=axs.flat[i*4])
+    
+        # Plot lineage probabilities
+        scatter(adata, color='silver', basis=basis, ax=axs.flat[i*4+1], show=False)
+        scatter(adata, basis=basis, 
+                       color=adata.uns['latent_dynamics']['posthoc_computations']['lineage_probs'][i],
+                       #adata.uns['latent_dynamics']['conditional_probabilities']['state_given_nodes'][adata.uns['latent_dynamics']['posthoc_computations']['condensed_latent_paths'][i]].sum(0),
+                       title='Chain {} cell probability'.format(i), 
+                       color_map='YlGnBu', vmin=0, vmax=1, show=False, ax=axs.flat[i*4+1])
+
+        # Plot hidden state history
+        plot_latent_state_history(adata, states=selected_states, chain=i, ax=axs.flat[i*4+2])
+        axs.flat[i*4+2].set_title('Hidden state probabilities chain {}'.format(i)) 
+
+        # Observed state probabilities
+        adata.uns['state_probability_sampling']['state_history_chain_{}'.format(i)] = adata.uns['latent_dynamics']['model_outputs']['joint_probabilities'].sum(1)[:,i,:]
+        plot_probability_by_clusters(adata, cluster_key=cluster_key, 
+                                     key='state_history_chain_{}'.format(i),
+                                     ax=axs.flat[i*4+3])
+        axs.flat[i*4+3].set_title('Observed state probabilities chain {}'.format(i))
+        
+    return fig, axs
+
+
+
+
+
+
+    
 
